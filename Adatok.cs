72a45a84f29457e5e0ade5f95ab3424704cf7927
Levi-24 <leventemajor36@gmail.com>
2024-02-19 11:14:56 +0100

    Add project files.

---
+++ Adatok.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villamlas
+{
+    internal class Adatok
+    {
+        public int nap { get; set; }
+        public int[] orankentiAdatok { get; set; }
+
+        public Adatok(int nap, string s)
+        {
+            var darabok = s.Split(';');
+            this.nap = nap;
+            this.orankentiAdatok = new int[darabok.Length];
+            for (int i = 0; i < darabok.Length; i++)
+            {
+                orankentiAdatok[i] = int.Parse(darabok[i]);
+            }
+        }
+
+        public override string ToString()
+        {
+            string adat = "";
+            for (int i = 0; i < orankentiAdatok.Length; i++)
+            {
+                adat += orankentiAdatok[i] + " ";
+            }
+
+            return $"{nap}. {adat};";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using Villamlas;
+
+List<Adatok> villamAdatok = new List<Adatok>();
+int nap = 1;
+
+using StreamReader sr = new StreamReader(@"../../../src/villam.txt");
+while (!sr.EndOfStream)
+{
+    villamAdatok.Add(new Adatok(nap, sr.ReadLine()));
+    nap++;
+}
+
+foreach (var item in villamAdatok)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("Feladat 3.:");
+var legtobbVillam = villamAdatok.Select(adatok => adatok.orankentiAdatok.Max()).Max();
+var legtobbVillamNap = villamAdatok
+               .Select(adat => new { Nap = adat.nap, VillamCount = adat.orankentiAdatok.Count(x => x == legtobbVillam) })
+               .OrderByDescending(x => x.VillamCount)
+               .FirstOrDefault();
+
+Console.WriteLine($"A legtöbb villám a {legtobbVillamNap.Nap}. napon volt: {legtobbVillam} villám.");
+
+Console.WriteLine("Feladat 4.:");
+Console.WriteLine("Kiírás.");
+var result = villamAdatok.Select(adat =>
+{
+    var elsoVillam = adat.orankentiAdatok.Select((villam, index) => new { Ora = index + 1, Villam = villam })
+        .FirstOrDefault(x => x.Villam != 0);
+
+    if (elsoVillam != null)
+        return $"{adat.nap}. {elsoVillam.Ora}";
+    else
+        return $"{adat.nap}. null";
+});
+
+using StreamWriter writer = new StreamWriter(@"../../../src/elsoVillamAdatok.txt");
+foreach (var item in result)
+{
+    writer.WriteLine(item);
+}
+
+Console.WriteLine("Feladat 5.:");
+var villamCount = villamAdatok.Select(adat => adat.orankentiAdatok.Count(x => x != 0));
+Console.WriteLine(villamCount.Sum());
+
+
+Console.WriteLine("Feladat 6.:");
+var villam200alat = villamAdatok
+               .Select(adat => new { Nap = adat.nap, VillamCount = adat.orankentiAdatok.Sum() })
+               .Where(villamCount => villamCount.VillamCount < 200)
+               .FirstOrDefault();
+Console.WriteLine(villam200alat.Nap);
+
+var villamMentesNap = villamAdatok
+               .Select(adat => new { Nap = adat.nap, VillamCount = adat.orankentiAdatok.Sum() })
+               .Where(villamCount => villamCount.VillamCount == 0)
+               .FirstOrDefault();
+Console.WriteLine("Feladat 7.:");
+if (villamMentesNap.Nap == null)
+{
+    Console.WriteLine("Nincs olyan nap amikor nem villámlott.");
+}
+else
+{
+    Console.WriteLine($"{villamMentesNap.Nap}. napon nem volt villámlás.");
+}
+
+
+Console.WriteLine();
+Console.WriteLine("----------Haladó Feladatok----------");
+Console.WriteLine();
+
+Console.WriteLine("Feladat 9.:");
+var villamSum = villamAdatok.Select(adat => adat.orankentiAdatok.Sum()).Take(20);
+Console.WriteLine(villamSum.Sum());
+
+Console.WriteLine("Feladat 11.:");
+var legtobbVillam7 = villamAdatok
+    .Select((villam, index) => new { Nap = villam.nap, VillamCount = villam.orankentiAdatok.Max(), Ora = index + 1 })
+    .Where(villamCount => villamCount.Nap == 7);
+foreach (var item in legtobbVillam7)
+{
+    Console.WriteLine(item.VillamCount);
+}
